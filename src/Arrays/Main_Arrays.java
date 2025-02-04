60f7b7e266b091fb1d922308c34cbd0691fb03c0
kiranmoorthy <126954759+kiranmoorthy@users.noreply.github.com>
2025-02-04 21:52:46 +0530

    commit 04/02/25

---
+++ src/Arrays/Main_Arrays.java
@@ -0,0 +1,7 @@
+package Arrays;
+
+public class Main_Arrays {
+    public static void main(String[] args) {
+        System.out.println("Main_Arrays");
+    }
+}
--- src/Numbers/Main_Numbers.java
+++ src/Numbers/Main_Numbers.java
@@ -1,10 +1,4 @@
-public class Main_Numbers
-{
-    public static void main(String[] args) {
-        System.out.print("Hello, World");
+package Numbers;
 
-
-
-    }
-
+public class Main_Numbers {
 }
