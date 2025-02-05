5e02813a9afcbd09633c158bc8ec7c2a628884c4
kiranmoorthy <126954759+kiranmoorthy@users.noreply.github.com>
2025-02-05 21:05:03 +0530

    commit 05/02/25

--- src/Strings/Main_Strings.java
+++ src/Strings/Main_Strings.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
+package Strings;
 
-public class Main_Strings{
+public class Main_Strings {
     public static void main(String[] args) {
-        System.out.println("Main String");
-
-
-
-
-
+        System.out.println("Main Strings");
     }
 }
